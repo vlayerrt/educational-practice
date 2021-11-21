a6756a3bf6f971632a59b7ff9954635e3c370fe6
vlad <39220694+vlayerrt@users.noreply.github.com>
2021-11-22 00:42:16 +0300

    Update Source.cpp

--- opencv/Source.cpp
+++ opencv/Source.cpp
@@ -88,14 +88,16 @@ string VideoCap() {
 	double dWidth = cap.get(CAP_PROP_FRAME_WIDTH); //get the width of frames of the video
 	double dHeight = cap.get(CAP_PROP_FRAME_HEIGHT); //get the height of frames of the video
 
-	cout << "Resolution of the video : " << dWidth << " x " << dHeight << endl;
+	
 
 	string window_name = "My Camera Feed";
 	namedWindow(window_name); //create a window called "My Camera Feed"
-
+	auto fps = cap.get(CAP_PROP_FPS); // frames per seconds
+	auto delay = 1000 / fps;
 	while (true)
 	{
 		Mat frame;
+		auto time = clock();
 		bool bSuccess = cap.read(frame); // read a new frame from video 
 
 		//Breaking the while loop if the frames cannot be captured
@@ -105,9 +107,30 @@ string VideoCap() {
 			cin.get(); //Wait for any key press
 			break;
 		}
+		
 
 		//show the frame in the created window
 		imshow(window_name, frame);
+
+		if (frame.channels() != 1) cvtColor(frame, frame, COLOR_RGB2GRAY);
+		Mat x, y;
+		Sobel(frame, x, CV_16S, 1, 0);
+		Sobel(frame, y, CV_16S, 0, 1);
+		// модуль собеля
+		convertScaleAbs(x, x);
+		convertScaleAbs(y, y);
+		addWeighted(x, 0.5, y, 0.5, 0, frame); //совмещаем картинки
+
+		imshow("sobel", frame);
+		// скорость видео
+		double time_work = (clock() - time) * 1000 / CLOCKS_PER_SEC;
+		if (time_work > delay - 1) time_work = 1;
+		else time_work = delay - time_work;
+
+		
+		////  если обработка собелем больше чем задержка тогда получится отрицательное значение(ошибка)
+		//int vidos = waitKey(time_work);
+		//if (vidos >= 0) break;
 
 		//wait for for 10 ms until any key is pressed.  
 		//If the 'Esc' key is pressed, break the while loop.
@@ -119,6 +142,9 @@ string VideoCap() {
 			break;
 		}
 	}
+	cout << "Resolution of the video : " << dWidth << " x " << dHeight << endl;
+	cout << "Задержка в мс = " << delay << endl;
+	waitKey(0);
 	return 0;
 }
 
@@ -218,42 +244,6 @@ string Video()
 
 }
 
-string VideoTH() {
-	setlocale(LC_ALL, "Russian");
-	VideoCapture vidos(0);
-	if (!vidos.isOpened()) cout << "Видео не найдено, проверьте указанный путь" << endl;
-
-	auto fps = vidos.get(CAP_PROP_FPS); // frames per seconds
-	auto delay = 1000 / fps;
-	cout << "Задержка в мс = " << delay << endl;
-
-	Mat frame;
-
-	while (1)
-	{
-		auto time = clock();
-		vidos >> frame; //считаем фрейм
-		if (frame.channels() != 1) cvtColor(frame, frame, COLOR_RGB2GRAY);
-		Mat x, y;
-		Sobel(frame, x, CV_16S, 1, 0);
-		Sobel(frame, y, CV_16S, 0, 1);
-		// модуль собеля
-		convertScaleAbs(x, x);
-		convertScaleAbs(y, y);
-		addWeighted(x, 0.5, y, 0.5, 0, frame); //совмещаем картинки
-		imshow("sobel", frame);
-		// скорость видео
-		double time_work = (clock() - time) * 1000 / CLOCKS_PER_SEC;
-		if (time_work > delay - 1) time_work = 1;
-		else time_work = delay - time_work;
-		//  если обработка собелем больше чем задержка тогда получится отрицательное значение(ошибка)
-		int vidos = waitKey(time_work);
-		if (vidos >= 0) break;
-	}
-	destroyAllWindows();
-	return 0;
-}
-
 int main()
 {
 	setlocale(LC_ALL, "Russian");
@@ -272,7 +262,7 @@ int main()
 		if (choose == "video") {
 			check = false;
 			//Video();
-			VideoTH();
+			VideoCap();
 		}
 		else {
 			check = true;
